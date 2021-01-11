9f79362b43b8013bdd7cc55c577fb15c8f312d79
lks0108 <dlrudtn108@gmail.com>
2021-01-12 01:04:04 +0900

    20210112

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -9,11 +11,28 @@ import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
 
-    public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+//    public MemberService memberService(){
+//        return new MemberServiceImpl(new MemoryMemberRepository());
+//    }
+
+
+    //    public OrderService orderService(){
+//        return new OrderServiceImpl(new MemoryMemberRepository(),new FixDiscountPolicy());
+//    }
+    public MemberService memberService() {
+        return new MemberServiceImpl(getMemberRepository());
     }
 
-    public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(),new FixDiscountPolicy());
+    private MemoryMemberRepository getMemberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(getMemberRepository(), discountPolicy());
+    }
+    //FixDiscountPolicy -> RateDiscountPolicy로 변경
+    public DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
